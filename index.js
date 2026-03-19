1248bcd2a2db30d4474559bc43fbbf667f2d5d0d
Home <home@192.168.1.38>
2026-03-19 21:42:37 +0530

    first completed files uploaded

---
+++ index.js
@@ -0,0 +1,299 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+dotenv = require('dotenv');
+dotenv.config();
+
+const port = 3000;
+
+const VERIFY_TOKEN = "my_verify_token";
+const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
+const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Server is running 🚀');
+});
+
+// 🔐 Webhook verification (GET)
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    console.log('WEBHOOK VERIFIED');
+    return res.status(200).send(challenge);
+  } else {
+    console.log('VERIFICATION FAILED');
+    return res.sendStatus(403);
+  }
+});
+
+const processedMessages = new Set();
+
+// ✅ Send List Function
+
+const listButton = async (to, text , options) =>{
+    try{
+        await axios.post(
+            `https://graph.facebook.com/v22.0/${PHONE_NUMBER_ID}/messages`,
+            {
+                messaging_product: 'whatsapp',
+                to,
+                type: 'interactive',
+                interactive : {
+                    type: 'list',
+                    body: {text},
+                    action : {
+                        button: 'View Options',
+                        sections :  [
+                            {
+                                title : "Services",
+                                rows : options.map(opt =>({
+                                    id: opt.id,
+                                    title: opt.title
+                                }))
+                            }
+                        ]
+                    }
+                }
+            },
+            {
+            headers: {
+          Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+          "Content-Type": "application/json"
+        }
+      }
+        )
+    }catch (error){
+        console.error("❌ Error sending list message:", error.response?.data || error.message);
+    }
+}
+
+// ✅ Send Button Function
+const sendButton = async (to, text, buttons) => {
+  try {
+    await axios.post(
+      `https://graph.facebook.com/v22.0/${PHONE_NUMBER_ID}/messages`,
+      {
+        messaging_product: 'whatsapp',
+        to,
+        type: 'interactive',
+        interactive: {
+          type: "button",
+          body: { text },
+          action: {
+            buttons: buttons.map(btn => ({
+              type: 'reply',
+              reply: {
+                id: btn.id,
+                title: btn.title
+              }
+            }))
+          }
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  } catch (error) {
+    console.error("❌ Error sending button:", error.response?.data || error.message);
+  }
+};
+const normalText = async (to, text) =>{
+    axios.post(
+        `https://graph.facebook.com/v22.0/${PHONE_NUMBER_ID}/messages`,
+        {
+          messaging_product: 'whatsapp',
+          to,
+          text: { body: text }
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+            "Content-Type": "application/json"
+          }
+        }
+      )
+      .catch(error => {
+        console.error("❌ Error sending text message:", error.response?.data || error.message);
+      });
+}
+
+app.post('/webhook', async (req, res) => {
+  try {
+    const value = req.body?.entry?.[0]?.changes?.[0]?.value;
+
+    // ❌ Ignore non-message events
+    if (!value?.messages) {
+      return res.sendStatus(200);
+    }
+
+    const message = value.messages[0];
+
+    if (processedMessages.has(message.id)) {
+      return res.sendStatus(200);
+    }
+    processedMessages.add(message.id);
+
+    const from = message.from;
+    const text = message.text?.body;
+
+    if (text) {
+      const msg = text.toLowerCase().trim();
+
+      console.log("From:", from);
+      console.log("Text:", text);
+
+      if (msg === "hi" || msg === "hello") {
+
+
+        await sendButton(
+          from,
+          `*Welcome to Shree Laxmi Infratech 🏢* \n\nWe provide expert assistance for:\n\n* Vidyut Suraksha (Electrical Safety)⚡\n\n* Lift & Escalator NOC Registration🛗 \n\n* Permanent Registration\n\n* NOC Renewal Services 🔄\n\n👉 Please click Menu to explore our services.\n\n*Our team will be happy to assist you 😄.*`,
+          [
+            { id: "menu_main", title: "📋 Menu" }
+          ]
+        );
+
+        return res.sendStatus(200);
+      }
+
+      if (msg === "help") {
+        await sendButton(
+          from,
+          `*Need Assistance?* 🤔\n\nYou can ask me about:\n\n* Vidyut Suraksha (Electrical Safety)⚡\n\n* Lift & Escalator NOC Registration🛗 \n\n* Permanent Registration\n\n* NOC Renewal Services 🔄\n\n👉 Please click Menu to explore our services.\n\n*Our team will be happy to assist you 😄.*`,
+          [
+            { id: "contact Us", title: "📋 Contact Us" }
+          ]
+        );
+
+        return res.sendStatus(200);
+      }
+      // fallback for unknown text
+      await axios.post(
+        `https://graph.facebook.com/v22.0/${PHONE_NUMBER_ID}/messages`,
+        {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "Sorry I didn't understand that 😔\n\nPlease send *hi* to start." }
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      return res.sendStatus(200);
+    }
+
+    const buttonReply = message?.interactive?.button_reply;
+    let choice1 = ''
+    let choice2 = ''
+    if(buttonReply){
+        console.log("Button Reply ID:", buttonReply.id);
+        if (buttonReply.id === 'menu_main'){
+            
+            listButton(
+                from,
+                `Select a service below to get started 👇\n\nWe’re here to assist you with all your approval and registration needs ⚡🛗.`,
+                [
+                    {id : "noc_renewal", title: 'Noc Renewal'},
+                    {id : "noc_registration", title: 'Noc Registation'},
+                    {id : "premise_registation", title: 'Premise Registration'},
+                    {id : "insurance", title: 'Apply for Insurance '},
+
+                ]
+                
+            )
+            
+        }
+
+    }
+
+    const listReply = message?.interactive?.list_reply;
+    if(listReply){
+        console.log("List Reply ID:", listReply.id);
+
+        
+        if(listReply.id === 'noc_renewal'){
+            
+            listButton(
+                from,
+                `Please select the type of NOC renewal you want to apply for:`,
+                [
+                    {id : 'transformer_renewal', title: 'Transformer NOC Renewal'},
+                    {id : 'DG_renewal', title: 'DG NOC Renewal'},
+                    {id : 'both-renewal-1-2', title: 'For 1st two options'},
+                    {id : 'lift_renewal', title: 'Lift NOC Renewal'},
+                    {id : 'escalator_renewal', title: 'Escalator NOC Renewal'},
+                    {id : 'last-two-noc', title: 'For last two options'},
+
+
+                ]
+            )
+        }
+        if(listReply.id === 'noc_registration'){
+           
+            listButton(
+                from,
+                `Please select the type of NOC registration you want to apply for:`,
+                [
+                    {id : 'transformer_registration', title: 'T/F NOC Registration'},
+                    {id : 'DG_registration', title: 'DG NOC Registration'},
+                    {id : 'both-registration-1-2', title: 'For 1st two options'},
+                    {id : 'lift_registration', title: 'Lift NOC Registration'},
+                    {id : 'escalator_registration', title: 'ESC NOC Registration'},
+                    {id : 'last-two-noc', title: 'For last two options'},
+
+
+                ]
+            )
+        }
+        if(listReply.id === 'premise_registation'){
+         
+            listButton(
+                from,
+                `Please select the type of * premise registration * you want to apply for:`,
+                [
+                    {id : 'lift_registration', title: 'Lift NOC Registration'},
+                    {id : 'escalator_registration', title: 'ESC NOC Registration'},
+                    {id : 'both-registration-1-2', title: 'For both options'},
+                ]
+            )
+        }
+        if(listReply.id === 'insurance'){
+            
+            listButton(
+                from,
+                `Please select the type of * insurance * you want to apply for:`,
+                [
+                    {id : 'lift_registration', title: 'Lift NOC Registration'},
+                    {id : 'escalator_registration', title: 'Escalator NOC Registration'},
+                    {id : 'both-registration-1-2', title: 'For both options'},
+                ]
+            )
+        }
+    }
+   
+
+  } catch (error) {
+    console.error("❌ Error:", error.response?.data || error.message);
+    return res.sendStatus(500);
+  }
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`🚀 Server running at http://localhost:${port}`);
+});
